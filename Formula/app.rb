75df30ec768e0eae9df2a8cd1efbd45ddb0d65bf
witx98 <witx90@gmail.com>
2025-02-12 00:09:57 +0100

    Update formula

--- Formula/app.rb
+++ Formula/app.rb
@@ -4,33 +4,37 @@ class App < Formula
   license "Apache-2.0"
   version "1.1.2"
 
-  if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/witx98/homebrew-test-logchange/releases/download/1.1.2/app-1.1.2-linuxx64.zip"
-    sha256 "50810c5f6f5674b39241d58ee0000945cea3f45c4444a0c6e4b837020bb5da31"
-    def install
-      libexec.install Dir["*"]
-      chmod 0755, "#{libexec}/app-1.1.0-linuxx64/app-1.1.0-runner"
-      bin.write_exec_script "#{libexec}/app-1.1.0-linuxx64/app-1.1.0-runner"
+  on_linux do
+    on_intel do
+      url "https://github.com/witx98/homebrew-test-logchange/releases/download/1.1.2/app-1.1.2-linuxx64.zip"
+      sha256 "50810c5f6f5674b39241d58ee0000945cea3f45c4444a0c6e4b837020bb5da31"
     end
   end
 
-  if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/witx98/homebrew-test-logchange/releases/download/1.1.0/app-1.1.0-darwinarm64.zip"
-    sha256 "dffe0097e40cb993b7203aedac6bb8070495d3068adf0f5ae94a256171e0a9e4"
-    def install
-      libexec.install Dir["*"]
-      chmod 0755, "#{libexec}/app-1.1.0-darwinarm64/app-1.1.0-runner"
-      bin.write_exec_script "#{libexec}/app-1.1.0-darwinarm64/app-1.1.0-runner"
+  on_macos do
+    on_arm do
+      url "https://github.com/witx98/homebrew-test-logchange/releases/download/1.1.2/app-1.1.2-darwinarm64.zip"
+      sha256 "37bab0d73dbcdda396963f9e7587f7d6843d70d6d595e7de565efb66d909d192"
+    end
+    on_intel do
+      url "https://github.com/witx98/homebrew-test-logchange/releases/download/1.1.2/app-1.1.2-darwinx64.zip"
+      sha256 "7e1cd3f2b433dd2159e7cba5fe811eedf61b8cf34f19c86e73e2a1fa0321b1e5"
     end
   end
 
-  if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/witx98/homebrew-test-logchange/releases/download/1.1.0/app-1.1.0-darwinx64.zip"
-    sha256 "feb0c7542e4aeb56eab44bb651affb3cfc7c3d26811865add4140116a573d29b"
-    def install
-      libexec.install Dir["*"]
-      chmod 0755, "#{libexec}/app-1.1.0-darwinx64/app-1.1.0-runner"
-      bin.write_exec_script "#{libexec}/app-1.1.0-darwinx64/app-1.1.0-runner"
+  def install
+    libexec.install Dir["*"]
+    os_arch = ""
+    if OS.linux? && Hardware::CPU.intel?
+      os_arch = "linuxx64"
+    elsif OS.mac? && Hardware::CPU.arm?
+      os_arch = "darwinarm64"
+    elsif OS.mac? && Hardware::CPU.intel?
+      os_arch = "darwinx64"
+    else
+      puts "Invalid system or architecture"
     end
+    chmod 0755, "#{libexec}/app-#{version}-#{os_arch}/app-#{version}-runner"
+    bin.write_exec_script "#{libexec}/app-#{version}-#{os_arch}/app-#{version}-runner"
   end
 end
