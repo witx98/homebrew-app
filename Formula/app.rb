3ffcdca2782c41e1ff4bc0809cfc02771b148bbd
witx98 <witxdev@gmail.com>
2025-02-16 17:02:43 +0000

    app v1.1.10

--- Formula/app.rb
+++ Formula/app.rb
@@ -1,20 +1,20 @@
 class App < Formula
   desc "CLI tool for managing changelogs"
   homepage "https://github.com/witx98/app"
-  version "1.1.9"
+  version "1.1.10"
   license "Apache-2.0"
 
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/witx98/app/releases/download/#{version}/app-linuxx64.zip"
-    sha256 "fa72a8079e84c0fe791c07b510b0642203eebc38a115fa07cc6797a76c723ce8"
+    sha256 "cf5b2e21bbb5fe846110aeb607a66d70242b3a1673d2bc7e0581ca8fa14b3bd4"
   end
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/witx98/app/releases/download/#{version}/app-darwinarm64.zip"
-    sha256 "8bbf0214174f2c1659b445e40e2b936d661860252d8fc9490e5ca41ad78ae4cd"
+    sha256 "4b9f682ff0fd0253af226e805d8ec502eb1200efafa04cf81b51c20c8b04b008"
   end
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/witx98/app/releases/download/#{version}/app-darwinx64.zip"
-    sha256 "c533baa29d8ed8765525412267e3b5d5071e659251f015fff111e92e14dfcb83"
+    sha256 "a699771c64e5a1e739349b3c0ec79f9240ca9d3447bd77e84976a4d5430c2d59"
   end
 
   def install
