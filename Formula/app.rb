e4705846b19e7ccc443aaca53154feafffa17179
witx98 <witxdev@gmail.com>
2025-02-16 12:48:03 +0000

    app v1.1.8

--- Formula/app.rb
+++ Formula/app.rb
@@ -1,20 +1,20 @@
 class App < Formula
   desc "A CLI tool for managing changelogs"
-  homepage "https://github.com/witx98/homebrew-test-logchange"
-  version "1.1.7"
+  homepage "https://github.com/witx98/app"
+  version "1.1.8"
   license "Apache-2.0"
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/witx98/homebrew-test-logchange/releases/download/#{version}/app-linuxx64.zip"
-    sha256 "a5696522adbfb25adc3d9c15c5ae6564c2aac7252af9a90fe19111f9ded457fa"
+    url "https://github.com/witx98/app/releases/download/#{version}/app-linuxx64.zip"
+    sha256 "2266f050fae74f742d5796fd1836875d133342ed6cfc301cdc50bdc9c8eb977a"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/witx98/homebrew-test-logchange/releases/download/#{version}/app-darwinarm64.zip"
-    sha256 "970c841000681666c7835e351dcc59558dcb79c36bc27e5d4fc27066b28bad6b"
+    url "https://github.com/witx98/app/releases/download/#{version}/app-darwinarm64.zip"
+    sha256 "96081c96968f5f08610af7ecd065a9bb048a50f6c8d4b09af3ee0afa68804a3c"
   end
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/witx98/homebrew-test-logchange/releases/download/#{version}/app-darwinx64.zip"
-    sha256 "ab9c3d777524b4bb7a08e093e629e73d0fe286e40e23785453f6e5cd61ef743d"
+    url "https://github.com/witx98/app/releases/download/#{version}/app-darwinx64.zip"
+    sha256 "73084b29438c3af55e5a0b93eb32a431e0328768b062fb55867d123449ceb7be"
   end
 
   def install
@@ -31,5 +31,10 @@ class App < Formula
     end
     chmod 0755, "#{libexec}/app-#{os_arch}/app-#{version}-runner"
     bin.write_exec_script "#{libexec}/app-#{os_arch}/app-#{version}-runner"
+  end
+
+  test do
+    output = shell_output("#{bin}/app --version")
+    assert_match version, output
   end
 end
